1476f7ca9ae30fee2e9c73be70d175960ed2bcd8
Harshal-Avate <harshalavate05@gmail.com>
2026-03-10 13:52:33 +0530

    Added product quantity system in cart

--- apps/products/views.py
+++ apps/products/views.py
@@ -6,7 +6,7 @@ from .models import Product
 def product_list(request):
     products = Product.objects.all()
     cart = request.session.get("cart", [])
-    cart_count = len(cart)
+    cart_count = sum(cart.values())
     context = {
         "products": products,
         "cart_count": cart_count
@@ -14,42 +14,55 @@ def product_list(request):
     return render(request, "products/product_list.html", context)
 
 def product_detail(request, id):
-    products = Product.objects.get.get_object_or_404(Product, id=id)
+    products = get_object_or_404(Product, id=id)
     context = {
         "product": products
     }
     return render(request, 'products/product_detail.html', {'product': products})
 
 def add_to_cart(request, id):
-    cart = request.session.get('cart', [])
-    cart.append(id)
-    request.session['cart'] = cart
-    return redirect('product_list')
+    cart = request.session.get("cart", {})
+
+    if isinstance(cart, list):
+        new_cart = {}
+        for item in cart:
+            new_cart[str(item)] = 1
+        cart = new_cart
+
+    id = str(id)
+
+    if id in cart:
+        cart[id] += 1
+    else:
+        cart[id] = 1
+
+    request.session["cart"] = cart
+    return redirect("cart")
 
 def cart_view(request):
-    cart = request.session.get('cart', [])
-    products = Product.objects.filter(id__in=cart)
-    return render(request, 'products/cart.html', {'products': products})
+    cart = request.session.get("cart", {})
 
-def cart_view(request):
-    cart = request.session.get("cart", []) #This gets the cart from the session.
-    products = Product.objects.filter(id__in=cart)
+    products = Product.objects.filter(id__in=cart.keys())
+
     total_price = 0
 
     for product in products:
-        total_price += product.price 
+        total_price += product.price * cart[str(product.id)]
 
     context = {
         "products": products,
+        "cart": cart,
         "total_price": total_price
     }
+
     return render(request, "products/cart.html", context)
 
 def remove_from_cart(request, id):
-    cart = request.session.get("cart", [])
+    cart = request.session.get("cart", {})
+    id = str(id)
 
     if id in cart:
-        cart.remove(id)
+        del cart[id]
 
     request.session["cart"] = cart
     return redirect("cart")
